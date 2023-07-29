79f2948e345bba30a12350b6ff0c24728fb02fc0
pragatichawre2000 <140889279+pragatichawre2000@users.noreply.github.com>
2023-07-29 16:15:18 +0530

    Add files via upload

---
+++ f31f34.c
@@ -0,0 +1,29 @@
+/*#include<stdio.h>
+int main()
+{
+	int samosa,vadapav,bill,rs;
+	printf("enter value samosa : ");
+	scanf("%d",&samosa);
+	printf("enter value vadapav : ");
+	scanf("%d",&vadapav);
+	printf("enter value rs : ");
+	scanf("%d",&rs);
+	bill=((samosa*rs)+(vadapav*rs));
+	printf("%d",bill);
+	return 0;
+}
+*/
+
+/*#include<stdio.h>
+int main()
+{
+	int a,b,p;
+	printf("enter value  a:");
+	scanf("%d",&a);
+	printf("enter value  b:");
+	scanf("%d",&b);
+	p=a/b;
+	printf("%d",p);
+	return 0;
+}
+*/
