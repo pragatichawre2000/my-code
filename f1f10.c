42e5b3ee50ea50993e9544a57c13db97bf2b3e0e
pragatichawre2000 <140889279+pragatichawre2000@users.noreply.github.com>
2023-07-29 16:12:16 +0530

    Add files via upload

---
+++ f1f10.c
@@ -0,0 +1,151 @@
+/*#include<stdio.h>
+int main()
+{
+   int a,b,c;
+   printf("enter a value a : ");
+   scanf("%d", &a);
+   printf("enter a value b : ");
+   scanf("%d", &b);
+   c=a+b;
+   printf("%d",c);
+   return 0;
+}
+*/
+
+/*#include<stdio.h>
+int main()
+{
+   int a,b,c;
+   printf("enter a value a : ");
+   scanf("%d", &a);
+   printf("enter a value b : ");
+   scanf("%d", &b);
+   c=(a+b)/2;
+   printf("%d",c);
+   return 0;
+}
+*/
+
+/*#include<stdio.h>
+int main()
+{
+   int DW,W,c;
+   printf("enter a value DW : ");
+   scanf("%d", &DW);
+   printf("enter a value W : ");
+   scanf("%d", &W);
+   c=DW*W;
+   printf("%d",c);
+   return 0;
+}
+*/
+
+/*#include<stdio.h>
+int main()
+{
+   int h,b,a;
+   printf("enter a value h : ");
+   scanf("%d", &h);
+   printf("enter a value b : ");
+   scanf("%d", &b);
+   a=h*b;
+   printf("%d",a);
+   return 0;   
+}
+*/
+
+/*#include<stdio.h>
+int main()
+{
+   int a,r,t,si;
+   printf("enter a value a : ");
+   scanf("%d", &a);
+   printf("enter a value r : ");
+   scanf("%d", &r);
+   printf("enter a value t : ");
+   scanf("%d", &t);
+   si=a*r*t;
+   printf("%d",si);
+   return 0;
+ }
+*/
+
+/*#include<stdio.h>
+int main()
+{
+   int interest, principle, number,gravity;
+   printf("enter a value interest : ");
+   scanf("%d", &interest);
+   printf("enter a value principle :");
+   scanf("%d",&principle); 
+   printf("enter value number : ");
+   scanf("%d",&number);
+   gravity =( (principle*(1+interest))/100^number);
+   printf("enter the gravity:%d",gravity);
+   return 0;
+}
+*/
+
+/*#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("enter value a :");
+	scanf("%d",&a);
+	printf("enter value b :");
+	scanf("%d",&b);
+	c=a;
+	a=b;
+	b=c;
+	printf("%d\n%d",a,b);
+	return 0;
+}
+*/
+
+/*#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("enter value a :");
+	scanf("%d",&a);
+	printf("enter value b :");
+	scanf("%d",&b);
+	a=a+b;
+	b=a-b;
+	a=a-b;
+	printf("%d\n%d",a,b);
+	return 0;
+}
+*/
+
+/*#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("enter value a :");
+	scanf("%d",&a);
+	printf("enter value b :");
+	scanf("%d",&b);
+	c=a*b;
+	printf("%d",c);
+	return 0;
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
