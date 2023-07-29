1fbd486a03767539013c4adef2f7a71175c9dc98
pragatichawre2000 <140889279+pragatichawre2000@users.noreply.github.com>
2023-07-29 16:44:32 +0530

    Add files via upload

---
+++ ifrun.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char city;
+	printf("enter value city :");
+	scanf("%s",&city);
+	
+	if (strcmp(city,"Delhi")==1)
+	{
+		printf("Redfort");
+	}
+	else if(city=="Agra")
+	{
+		printf("Tajmahal");
+	}
+	else if(city=="Jaipur")
+	{
+		printf("Jalmahal");
+	}
+	else
+	{
+		printf("invalid");
+	}
+	return 0;
+}
+
+#include<stdio.h>
+int main()
+{
+	int a,s,d,f,g;
+	printf("enter value a,s,d,f,g");
+	scanf("%d%d%d%d",&a,&s,&d &f,&g");
+	if(a>=18)
+	{
+	 	if(a<30)
+		{
+			if(s==m)
+			{
+				printf("%d",700*d");
+			}
+				else if(s==f)
+				{
+					printf("%d",750*d");
+				}
+				
+#include<stdio.h>
+int main()
+{
+	char;
+	if(>a && <=Z || a>A && a<Z)
+	{
+		printf("it is Alphabet");
+	}
+	else
+	{
+		printf(("it is not Alphabet");
+	}
+	return 0;
+}
+		}
+
+
+
+
+
+
