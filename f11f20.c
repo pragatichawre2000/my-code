d0befdd7932b66e09179284a1b6fda1563347eb9
pragatichawre2000 <140889279+pragatichawre2000@users.noreply.github.com>
2023-07-29 16:13:23 +0530

    Add files via upload

---
+++ f11f20.c
@@ -0,0 +1,136 @@
+/*#include<stdio.h>
+int main(){
+    int b,h,area;
+    printf("enter a value b : ");
+    scanf("%d",&b);
+    printf("enter a value h : ");
+    scanf("%d",&h);
+    area=(b*h)/2;
+    printf("%d",area);
+    return 0;
+}
+*/
+
+/*#include<stdio.h>
+int main(){
+    int side1,side2,area;
+    printf("enter a value side1 : ");
+    scanf("%d",&side1);
+    printf("enter a value side2 : ");
+    scanf("%d",&side2);
+    area=(side1*side2);
+    printf("%d",area);
+    return 0;
+}
+*/
+
+/*#include<stdio.h>
+int main()
+{
+	int side1,side2,side3,semiperimiter;
+	printf("enter value side1 : ");
+	scanf("%d",&side1);
+	printf("enter value side2 : ");
+	scanf("%d",&side2);
+	printf("enter value side3 : ");
+	scanf("%d",&side3);
+	semiperimiter = (side1+side2+side3)/2;
+	printf("%d",semiperimiter);
+	return 0;
+}
+*/
+
+/*#include<stdio.h>
+int main()
+{
+  	float pi,r,area;
+  	pi=3.14;
+  	printf("enter the vqlue of r : ");
+  	scanf("%f",&r);
+  	area=(3.14*r*r);
+  	printf("%f",area);
+  	return 0;
+}
+*/
+
+/*#include<stdio.h>
+int main()
+{	
+	int height,length,ls,ts;
+	printf("enter value length : ");
+	scanf("%d",&length);
+	ls=(4*(length*length));
+	ts=(6*(length*length));
+	printf("%d\n %d",ls,ts);
+	return 0;
+}
+*/
+
+/*#include<stdio.h>
+int main()
+{
+	int lenght,width,height,cuboid;
+	printf("enter value lenght : ");
+	scanf("%d",&lenght);
+	printf("enter value width : ");
+	scanf("%d",&width);
+	printf("enter value height : ");
+	scanf("%d",&height);
+	cuboid=lenght*height*width;
+	printf("%d",cuboid);
+	return 0;
+}
+*/
+
+/*#include<stdio.h>
+int main()
+{
+	int w, h,l, surface_area;
+	printf("enter value heigth:");
+	scanf("%d",&h);
+	printf("enter value lenght: ");
+	scanf("%d",&l);
+	printf("enter value width : ");
+	scanf("%d",&w);
+	surface_area=2*(l*h)+(l*w)+(w*h);
+	printf("%d",surface_area);
+	return 0;
+}
+*/
+
+/*#include<stdio.h>
+int main()
+{
+	int number,c;
+	printf("enter value number:");
+	scanf("%d",&number);
+	c=number%10;
+	printf("%d",c);
+	return 0;
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
