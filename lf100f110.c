5649742a8ad9c0226556027d945716df44aa623f
pragatichawre2000 <140889279+pragatichawre2000@users.noreply.github.com>
2023-07-29 16:46:09 +0530

    Add files via upload

---
+++ lf100f110.c
@@ -0,0 +1,124 @@
+/*#include<stdio.h>
+int main()
+{	
+	int n,r;
+	int s=0;
+	scanf("%d %d",&n,&r);
+	for(n>0)
+	{
+		r=n%10;
+		s=s+r;
+	}
+	n=n/10;
+	printf("%d",s);
+	return 0;
+}
+*/	
+			
+/*#include<stdio.h>
+int main()
+{			\\f102
+	int n;
+	scanf("%d",&n);
+	for(int i=1; i<n/2; i++);
+	{
+		if(n%i==0)
+		{
+			printf("%d",i);
+		}
+	}
+	
+}	
+*//
+
+
+/*#include<stdio.h>
+int main()				//f103
+{
+	int n;
+	scanf("%d",&n);
+	int s=0;
+	for(int i<10000; i++)
+	{
+		if(i%2==0);
+		{
+			s=s+i;
+		}
+	}
+	else
+	{	
+		if(s==n)
+		{
+			profitf("%d", perfect no.);
+		}
+		else
+		{
+			printf("%d", not perfect no.);
+		}
+	}
+	
+	
+	return 0;
+}
+*/
+
+/*#include<stdio.h>
+int main()
+{				\\f106							
+	int n;
+	scanf("%d",&n);
+	int s=0;
+	int p=0;
+	for(int i=1; i<n; i++)
+	{
+		p=(p+(5*10)*i);
+		s=s+p;
+	}
+	i=i+1;
+	printf("%d",s);
+	return 0;
+}
+*/
+					
+/*#include<stdio.h>
+int main()
+{
+	int n,m, p;
+	scanf("%d%d%d",&n,&p,&m);
+	p=3;
+	for(int i=1; i<n; i++)
+	{
+		m=p*i;
+	}
+	printf("%d", m);
+	return 0;
+}
+*/		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+				
+
+
+
